28086d478825967dd2e476b7d7926380cee71b99
kaolive <karolina.oliveira@daimlertruck.com>
2025-06-08 18:17:46 +0100

    fix: change to the correct route table association

--- routes.tf
+++ routes.tf
@@ -56,5 +56,5 @@ resource "aws_route_table_association" "private" {
   count = length(var.private_subnets)
 
   subnet_id      = aws_subnet.private[count.index].id
-  route_table_id = aws_route.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
 }
